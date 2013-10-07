f439a181675dc3d9cd69c1098ce4c6841eec8e18
anshaoyuan <anshaoyuan@gmail.com>
2013-10-07 15:05:09 +0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails4::Application.routes.draw do
   
+  resources :microposts
+
+  resources :users
+  resources :sessions, only:[:new, :create,:destroy]
   root to:'static_pages#home'
  
   match '/help', to: 'static_pages#help',via:'get'
@@ -7,9 +11,8 @@ Rails4::Application.routes.draw do
   match '/contact',to:'static_pages#contact',via:'get'
  
   match '/signup',  to: 'users#new',            via: 'get'
-  resources :microposts
-
-  resources :users
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to:'sessions#destroy',      via:  'delete' 
  # config.serve_static_assets = true
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
